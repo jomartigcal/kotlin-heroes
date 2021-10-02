840b6da9821b7d9c4c768ef1de17cbb54c511c4c
Jomar Tigcal <jomar@tigcal.com>
2021-10-01 20:58:29 -0700

    Adding accepted submissions for Kotlin Heroes: Practice 8 (A)

---
+++ practice-8/README.md
@@ -0,0 +1,2 @@
+# Kotlin Heroes: Practice 8
+[https://codeforces.com/contest/1570](https://codeforces.com/contest/1570)
---
+++ practice-8/src/main/kotlin/practice8a.kt
@@ -0,0 +1,14 @@
+//  8A
+//  https://codeforces.com/contest/1570/problem/A
+
+private fun readLn() = readLine()!!
+private fun readInt() = readLn().toInt()
+private fun readStrings() = readLn().split(" ")
+private fun readInts() = readStrings().map { it.toInt() }
+
+fun main() {
+    repeat(readInt()) {
+        val numbers = readInts()
+        println(numbers[0] + numbers[1])
+    }
+}
